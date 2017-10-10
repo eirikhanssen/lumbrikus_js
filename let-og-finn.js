bab67d4eefa58948370b9e1e0277416b383bfc6e
Eirik Hanssen <eirikhanssen79@gmail.com>
2017-10-10 13:53:03 +0200

    add workaround for IE browsers that don't support paint-order

--- let-og-finn.js
+++ let-og-finn.js
@@ -1,5 +1,5 @@
 /*jslint browser: true */
-/*global window, Snap */
+/*global window, Snap, alert */
 function init() {
     'use strict';
     console.log("let-og-finn init");
@@ -7,6 +7,55 @@ function init() {
     var group_active_zones = s.select("#g_active_zones");
     //console.log(group_active_zones);
     group_active_zones.addClass('show');
+
+    function detectIE() {
+        var ua = window.navigator.userAgent;
+        /* Test values;
+        // IE 10
+        // ua = 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)';
+        // IE 11
+        // ua = 'Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko';
+        // Edge 12 (Spartan)
+        // ua = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36 Edge/12.0';
+        // Edge 13
+        // ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2486.0 Safari/537.36 Edge/13.10586';
+        */
+        var msie = ua.indexOf('MSIE ');
+        if (msie > 0) {
+        // IE 10 or older => return version number
+            return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+        }
+
+        var trident = ua.indexOf('Trident/');
+        if (trident > 0) {
+        // IE 11 => return version number
+            var rv = ua.indexOf('rv:');
+            return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+        }
+
+        var edge = ua.indexOf('Edge/');
+        if (edge > 0) {
+        // Edge (IE 12+) => return version number
+            return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+        }
+
+        // other browser
+        return false;
+    }
+
+    var msie_version = detectIE();
+
+    // workaround for browsers not supporting paint-order, namely internet explorer and edge among newer browsers
+    if (msie_version !== false) {
+        alert('msie detected!');
+        var words = s.selectAll('#g_ord > text, #g_ord > use');
+        var wordclones_container = s.select('#g_ord_clones');
+        words.forEach(function (el) {
+            var use = el.use();
+            wordclones_container.append(use);
+        });
+    }
+
     var zones = group_active_zones.selectAll('path');
     zones.forEach(function (el) {
         el.click(function () {
